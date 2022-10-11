f43da7368cae0a39aaddf129a26100294ae571ce
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-11 18:29:12 +0200

    Test Driven Dev

---
+++ lib/word_counter.rb
@@ -0,0 +1,30 @@
+dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+def word_counter(str,dictionnary)
+    my_Words_Array = str.split
+    dictionnary_capitalize = dictionnary.map do |word|
+        word.capitalize 
+    end
+    my_dictionnary = []
+    my_words_count = []
+    for x in 0...dictionnary.length
+        count = 0
+        for i in 0...my_Words_Array.length
+            if my_Words_Array[i].include? dictionnary[x]
+                count += 1
+            elsif my_Words_Array[i].include? dictionnary_capitalize[x]
+                count += 1
+            end
+        end
+        if count != 0
+            my_dictionnary.append(dictionnary[x])
+            my_words_count.append(count)
+        end
+    end
+    return Hash[my_dictionnary.zip my_words_count]
+end
+
+print word_counter("below",dictionnary)
+#print word_counter("Howdy partner, sit down! How's it going?",dictionnary)
+# print word_counter("Jean-Baptiste, aime le saucisson! but? he prefer to go lowness spoutnit pingoing down with his partner's to horn childrens at school", dictionnary)
+# puts
